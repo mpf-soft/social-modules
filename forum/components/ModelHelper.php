8e5ec929808d8357158081ad76a0a774e359d705
Mirel Mitache <mirel.mitache@avira.com>
2015-04-01 17:23:07 +0300

    Starting to work on custom models

--- forum/components/ModelHelper.php
+++ forum/components/ModelHelper.php
@@ -9,6 +9,25 @@ namespace mpf\modules\forum\components;
 
 use mpf\base\Object;
 
+/**
+ * Class ModelHelper
+ * Allows developer to extend the current models used  by forum module.
+ * There is no option here for controllers as they can easily be extended using controllerAliases option from WebApp class config.
+ * Example for controller in config:
+ *  $config['WebApp']['controllerAliases'] = [
+ *      'forum/user' => '\app\forum-extenders\controllers\User'
+ *  ];
+ * @package mpf\modules\forum\components
+ */
 class ModelHelper extends Object{
-
+    public $models = [
+        'ForumCategory' => '\mpf\modules\forum\models\ForumCategory',
+        'ForumReply' => '\mpf\modules\forum\models\ForumReply',
+        'ForumSection' => '\mpf\modules\forum\models\ForumSection',
+        'ForumSubcategory' => '\mpf\modules\forum\models\ForumSubcategory',
+        'ForumThread' => '\mpf\modules\forum\models\ForumThread',
+        'ForumTitle' => '\mpf\modules\forum\models\ForumTitle',
+        'ForumUser2Section' => '\mpf\modules\forum\models\ForumUser2Section',
+        'ForumUserGroup' => '\mpf\modules\forum\models\ForumUserGroup'
+    ];
 }
