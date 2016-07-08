68c572c79e49c3c7751399be8c14ccfa055a79cf
Mirel <mirel.mitache@avira.com>
2016-07-08 13:47:27 +0300

    Fix file upload

--- blog/views/pages/articles/edit.php
+++ blog/views/pages/articles/edit.php
@@ -3,6 +3,7 @@
     'name' => 'save',
     'model' => $model,
     'theme' => 'default-wide',
+    'formHtmlOptions' => ['enctype' => 'multipart/form-data'],
     'fields' => \mpf\modules\blog\models\BlogPost::getFormFields()
 ])->display(); ?>
 <?php require_once dirname(dirname(__DIR__)) . '/layout/footer.php'; ?>
